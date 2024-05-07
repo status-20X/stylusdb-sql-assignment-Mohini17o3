5ef1ef8a48e3c26bdd5f027eb089b06887c036a4
Mohini <upretimohini@gmail.com>
2024-05-07 22:16:01 +0530

    Test 1 and 2 passed (trial)

--- tests/step-02/index.test.js
+++ tests/step-02/index.test.js
@@ -1,6 +1,6 @@
 const readCSV = require('../../src/csvReader');
 
-test('Read CSV File', async () => {
+test('csvReader.js', async () => {
     const data = await readCSV('./sample.csv');
     expect(data.length).toBeGreaterThan(0);
     expect(data.length).toBe(3);
